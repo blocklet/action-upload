29fad7b03c10e7f25ae128cf8252da1ec8b4d02d
LancelotLewis <lancelot_lewis@163.com>
2021-10-18 22:30:48 +0800

    v0.2.4

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const core = require('@actions/core');
-const exec = require('@actions/exec');
+const shell = require('shelljs');
 
 const skip = core.getInput('skip');
 if (skip === 'true') {
@@ -10,43 +10,42 @@ if (skip === 'true') {
 }
 
 (async () => {
-  const file = path.join(process.cwd(), '.blocklet/release/blocklet.json');
-  if (!fs.existsSync(file)) {
-    throw new Error('Missing file at .blocklet/release/blocklet.json');
-  }
+  const workingDirectory = core.getInput('workgin-directory');
+  const endpoint = core.getInput('endpoint', { required: true });
+  const accessToken = core.getInput('access-token');
+  const developerSk = core.getInput('developer-sk');
+  const filePath = core.getInput('file-path');
 
   try {
+    const cdRes = shell.cd(workingDirectory);
+    if (cdRes.code !== 0) {
+      throw new Error(`Failed to change directory to ${workingDirectory}`);
+    }
+
+    const file = path.join(process.cwd(), filePath);
+    if (!fs.existsSync(file)) {
+      throw new Error('Missing file at .blocklet/release/blocklet.json');
+    }
     console.log('Uploading using github action');
-    const endpoint = core.getInput('endpoint', { required: true });
-    const accessToken = core.getInput('access-token');
-    const developerSk = core.getInput('developer-sk');
+
     if (!(accessToken || developerSk)) {
       throw new Error('Missing access token or developer sk');
     }
-    await exec.exec(`blocklet config set registry ${endpoint}`, [], {
-      listeners: {
-        stderr(err) {
-          throw new Error(err.toString());
-        },
-      },
-    });
+    const configRes = shell.exec(`blocklet config set registry ${endpoint}`);
+    if (configRes.code !== 0) {
+      throw new Error(`Failed to set registry: ${configRes.stderr}`);
+    }
 
     if (accessToken) {
-      exec.exec(`blocklet upload --access-token ${accessToken}`, [], {
-        listeners: {
-          stderr(err) {
-            throw new Error(err.toString());
-          },
-        },
-      });
+      const uploadRes = shell.exec(`blocklet upload ${file} --access-token ${accessToken}`);
+      if (uploadRes.code !== 0) {
+        throw new Error(`Failed to upload: ${uploadRes.stderr}`);
+      }
     } else {
-      exec.exec(`blocklet publish --developer-sk ${developerSk}`, [], {
-        listeners: {
-          stderr(err) {
-            throw new Error(err.toString());
-          },
-        },
-      });
+      const publishRes = shell.exec(`blocklet publish ${file} --developer-sk ${developerSk}`);
+      if (publishRes.code !== 0) {
+        throw new Error(`Failed to publish: ${publishRes.stderr}`);
+      }
     }
     console.log(`Upload blocklet to ${endpoint} success!`);
   } catch (error) {
